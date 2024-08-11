2f89a3e1284cadcf2c3010462058b925010a5cf3
Hemasundar0810 <nukahemasundar@gmail.com>
2024-08-12 00:40:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -93,10 +93,10 @@ if st.button("Get Detailed Notes"):
                 if target_language!='en':
                     summary = generate_gemini_content(transcript_text, prompt)
                     summary=translate_summary(summary, target_language)
-             else:
-                 summary = generate_gemini_content(transcript_text, prompt)
-             st.markdown("<h2 style='color: #FF6347;'>Detailed Notes:</h2>", unsafe_allow_html=True)
-             st.write(summary)
+            else:
+                summary = generate_gemini_content(transcript_text, prompt)
+            st.markdown("<h2 style='color: #FF6347;'>Detailed Notes:</h2>", unsafe_allow_html=True)
+            st.write(summary)
         
     except Exception as e:
         st.error("Sorry, we couldn't retrieve the transcript for this video. It might not be available or there could be an issue with the connection.")
